9c7c1325f0da56616acf337364e90d7bfc914e34
DevJohann <felipepoveda273@gmail.com>
2024-05-01 19:21:40 -0500

    Initial commit

---
+++ src/test/java/com/rootsoftworks/course/di/invoice/springdiinvoice/SpringDiInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootsoftworks.course.di.invoice.springdiinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
